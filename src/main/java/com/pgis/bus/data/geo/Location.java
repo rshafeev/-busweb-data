6d66f424def7d69f7248372c2c1fc14511997352
Roman.Shafeyev <rs@premiumgis.com>
2012-08-05 12:41:36 +0300

    add Location class

---
+++ src/main/java/com/pgis/bus/data/geo/Location.java
@@ -0,0 +1,19 @@
+package com.pgis.bus.data.geo;
+
+/**
+ * @author romario
+ * 
+ */
+public class Location {
+	public double lat;
+	public double lon;
+	
+	public Location(){
+		
+	}
+	public Location(double lat, double lon) {
+		this.lat = lat;
+		this.lon = lon;
+	}
+	
+}
